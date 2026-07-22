a7da1bbe9769894e437244417942376f80a948c1
Gytis Norutis <gytis.norutis@outlook.com>
2026-07-22 18:46:10 +0100

    Fix: include frontend files properly

---
+++ frontend/src/app/features/task-list/task-list.component.ts
@@ -0,0 +1,40 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+type TaskStatus = 'todo' | 'in-progress' | 'completed' | 'blocked' ;
+
+interface Task {
+  id: number;
+  title: string;
+  status: TaskStatus;
+}
+
+@Component({
+  selector: 'app-task-list',
+  standalone: true,
+  imports: [FormsModule, NgClass],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.scss'
+})
+export class TaskListComponent {
+  newTaskTitle: string = '';
+  taskStatus: TaskStatus = 'todo';
+
+  addTask() {
+    if (this.newTaskTitle.trim()) {
+      this.tasks.push({ id: this.tasks.length + 1, title: this.newTaskTitle, status: this.taskStatus });
+      this.newTaskTitle = '';
+    }
+  }
+
+  deleteTask(taskId: number) {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  tasks: Task[] = [
+    { id: 1, title: 'Task 1', status: 'todo' },
+    { id: 2, title: 'Task 2', status: 'todo' },
+    { id: 3, title: 'Task 3', status: 'todo' },
+  ]
+}
